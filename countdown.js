2469fa399827480846e9d896a278bcdd48df85be
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-26 14:16:18 +0300

    fix function

--- countdown.js
+++ countdown.js
@@ -1,12 +1,12 @@
 "use strict";
 const timer = (deadline) => {
-  // функция склонение числительных
-  const declensionNum = (num, words) => {
-    return words[
-      num % 100 > 4 && num % 100 < 20
-        ? 2
-        : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]
-    ];
+  const declensionNum = (value, words) => {
+    value = Math.abs(value) % 100;
+    let num = value % 10;
+    if (value > 10 && value < 20) return words[2];
+    if (num > 1 && num < 5) return words[1];
+    if (num == 1) return words[0];
+    return words[2];
   };
   const zeroFormat = (x) => {
     if (x < 10) {
